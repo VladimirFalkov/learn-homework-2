5e0330f0f23d99b6b6a159de7fc4ebd59d487c73
Vladimir Falkov <vladimirfalkov585@gmail.com>
2022-05-27 13:17:58 +0300

    Решение

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -1,3 +1,5 @@
+ 
+from datetime import datetime, date, timedelta
 """
 Домашнее задание №2
 
@@ -9,12 +11,14 @@
 """
 
 def print_days():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
-
+    today_date = date.today()
+    delta = timedelta(days=1)
+    yeasterday_date = today_date - delta
+    date_30_days_ago = today_date - 30*delta
+    today_date.strftime("%d.%m.%Y")
+    print(f'Сегодняшняя дата: {yeasterday_date.strftime("%d.%m.%Y")}')
+    print(f'Сегодняшняя дата: {today_date.strftime("%d.%m.%Y")}')
+    print(f'Дата 30 дней назад была: {date_30_days_ago.strftime("%d.%m.%Y")}')
 
 def str_2_datetime(date_string):
     """
@@ -25,4 +29,4 @@ def str_2_datetime(date_string):
 
 if __name__ == "__main__":
     print_days()
-    print(str_2_datetime("01/01/20 12:10:03.234567"))
+    #print(str_2_datetime("01/01/20 12:10:03.234567"))
